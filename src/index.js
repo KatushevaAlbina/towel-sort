4f1a3d9ea91e2c5c117baa55c2f526293523f26f
Katushev Albina <atuktarova83@gmail.com>
2023-08-28 18:07:38 +0300

    feat: add solved the towel-sorl algoritmik

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix=[]) {
+  if (matrix.length == 0) {
+    return [];
+  }
+  let result = [];
+  matrix.forEach((arr, i) => {
+    if (i % 2 == 0) {
+      arr.forEach(element => {
+        result.push(element);
+      });
+    } else {
+      arr.reverse().forEach(element => {
+        result.push(element);
+      });
+    }
+  });
+  return result;
 }
